add86ec7c4a352035d67f3b89d7154fff79da57f
Denis <122615902+ssteryally@users.noreply.github.com>
2023-09-12 16:15:45 +0300

    Add files via upload

---
+++ internalfilestorage/src/main/java/ru/mirea/shunyaev/internalfilestorage/MainActivity.java
@@ -0,0 +1,39 @@
+package ru.mirea.shunyaev.internalfilestorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import java.io.FileOutputStream;
+
+import ru.mirea.shunyaev.internalfilestorage.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private String fileName = "mirea.txt";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FileOutputStream outputStream;
+                try {
+                    String string = binding.editText.getText().toString();
+                    outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
+                    outputStream.write(string.getBytes());
+                    outputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
